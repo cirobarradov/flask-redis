f179a72c13e2c6b6fc992a748094830e56d0bebf
Ricardo Bravo <rbravo@datiobd.com>
2017-07-13 14:37:48 +0200

    Update publisher.py

--- app/publisher.py
+++ app/publisher.py
@@ -7,17 +7,14 @@ api = Api(app)
 
 jobs = {}
 
-class PublishJobs(Resource):
-
-    def put(self, job_id):
-        jobs[job_id] = request.form['data']
-        #print(request.form['redis'])
-        connection = redis.StrictRedis(host=os.environ.get('REDIS_SERVER'), port=6379, db=0)
-        #connection.publish('jobs', json.loads(jobs[job_id]))
-        connection.publish(os.environ.get('CHANNEL'), jobs[job_id])
-        return {job_id: jobs[job_id]}
-
-api.add_resource(PublishJobs, '/<string:job_id>')
+@app.route("/", methods=['POST'])
+def publishJobs():
+    job=request.form.to_dict()
+    #print(request.form['redis'])
+    connection = redis.StrictRedis(host=os.environ.get('REDIS_SERVER'), port=6379, db=0)
+    #connection.publish('jobs', json.loads(jobs[job_id]))
+    connection.publish(os.environ.get('CHANNEL'), job)
+    return str(job)
 
 if __name__ == '__main__':
     app.run(debug=True)
