71ba76541b5e303430dc94d24d890f6813e74b61
Ricardo Bravo <rbravo@datiobd.com>
2017-07-14 10:04:48 +0200

    Add files via upload

--- app/publisher.py
+++ app/publisher.py
@@ -7,6 +7,7 @@ api = Api(app)
 
 jobs = {}
 
+
 @app.route("/", methods=['POST'])
 def publishJobs():
     job=request.form.to_dict()
@@ -14,7 +15,23 @@ def publishJobs():
     connection = redis.StrictRedis(host=os.environ.get('REDIS_SERVER'), port=6379, db=0)
     #connection.publish('jobs', json.loads(jobs[job_id]))
     connection.publish(os.environ.get('CHANNEL'), job)
+
     return str(job)
+
+'''
+class PublishJobs(Resource):
+
+    def put(self, job_id):
+        jobs[job_id] = request.form['data']
+        #print(request.form['redis'])
+        connection = redis.StrictRedis(host=os.environ.get('REDIS_SERVER'), port=6379, db=0)
+        #connection.publish('jobs', json.loads(jobs[job_id]))
+        connection.publish(os.environ.get('CHANNEL'), jobs[job_id])
+        return {job_id: jobs[job_id]}
+
+api.add_resource(PublishJobs, '/<string:job_id>')
+'''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
